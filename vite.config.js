da88fee968bb0f30e1de42409b87b9abdce07121
YifanZhao1126 <yifan.zhao3@mail.mcgill.com>
2026-02-03 01:59:56 -0500

    Clean up vite config

--- vite.config.js
+++ vite.config.js
@@ -9,14 +9,7 @@ export default defineConfig({
   },
   build: {
     target: 'es2015',
-    outDir: 'docs',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js'
-      }
-    }
+    outDir: 'docs'
   },
   plugins: [{
     name: 'create-nojekyll',
